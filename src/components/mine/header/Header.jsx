48c755b3524cf13be7b2164ffb1bc4e91369de88
王承毅 <44698391+wangchenyi1996@users.noreply.github.com>
2020-08-31 15:04:37 +0800

    Update Header.jsx

--- src/components/mine/header/Header.jsx
+++ src/components/mine/header/Header.jsx
@@ -10,6 +10,8 @@ import s_vip from '@/assets/imgs/login/vip.png'
 import s_vip2 from '@/assets/imgs/login/vip2.jpg'
 import s_face from '@/assets/imgs/login/face.jpg'
 
+import { debounce2 } from '@/config/utils'
+
 class Header extends Component {
 
     state = {
@@ -17,11 +19,16 @@ class Header extends Component {
         userInfo:{
             isLogin:true,
             mobile: '18112646778'.replace(/(\d{3})(\d{4})(\d{4})/, '$1****$3')
-        }
+        },
+        //控制渐变标题栏的参数
+        beforeHeaderzIndex: 11, //层级
+        afterHeaderzIndex: 10, //层级
+        beforeHeaderOpacity: 1, //不透明度
+        afterHeaderOpacity: 0, //不透明度
     }
 
     componentDidMount(){
-        window.addEventListener("scroll", this.handleScroll);
+        window.addEventListener("scroll", debounce2(this.handleScroll,10,true));
     }
 
     componentWillUnmount(){
@@ -29,52 +36,41 @@ class Header extends Component {
     }
 
     // 处理滚动
-    handleScroll = ()=> {
-        const top = document.documentElement.scrollTop;
-        if (top > 90) {
+    handleScroll = (e)=> {
+        // console.log(e.target.documentElement.scrollTop)
+        let top = document.documentElement.scrollTop;
+        //导航栏渐变
+        let tmpY = 90;
+        top = top > tmpY ? tmpY : top;
+        console.log(top * (1 / tmpY))
+        if(top>0){
             this.setState({
-                isShow:false
+                afterHeaderOpacity: top * (1 / tmpY),
+                beforeHeaderOpacity: 1 - this.afterHeaderOpacity,
+                beforeHeaderzIndex: 10,
+                afterHeaderzIndex: 11
             })
-        } else {
+        }else{
             this.setState({
-                isShow:true
+                afterHeaderOpacity: top * (1 / tmpY),
+                beforeHeaderOpacity: 1 - this.afterHeaderOpacity,
+                beforeHeaderzIndex: 11,
+                afterHeaderzIndex: 10
             })
         }
     }
-
-    // 跳转到消息列表页面
-    goMsg = ()=> {
-        const { history } = this.props
-        history.push('/informs') 
-    }
-
-    // 跳转到设置页面
-    goSet = ()=> {
-        const { history } = this.props
-        history.push('/set') 
-    }
-
-    // 跳转到登录页面
-    goLogin = ()=> {
-        const { history } = this.props
-        history.push('/login') 
-    }
-
     isShowComponent =()=>{
-        let { isShow, userInfo } = this.state
-        if(isShow){
-            return (
-               <div className="headers">
+        let {userInfo,afterHeaderOpacity,beforeHeaderzIndex,afterHeaderzIndex } = this.state
+        return (
+            <>
+               <div className="headers" style={{ opacity: 1 - afterHeaderOpacity, zIndex: beforeHeaderzIndex }}>
                     <div className="header u-f u-f-sbc">
                         <img src={ s_img } alt="设置图标" onClick={ this.goSet }/>
                         <i className="iconfont icon-xiaoxi iconmsg" onClick={ this.goMsg }></i>
                         { userInfo.isLogin ? <span className="msg-dot"></span> : '' }
                     </div>
-               </div>
-            )
-        }else{
-            return (
-                <div className="fix-header u-f u-f-sbc">
+                </div>
+               <div className="fix-header u-f u-f-sbc" style={{ opacity: afterHeaderOpacity, zIndex: afterHeaderzIndex }}>
                     <div className="users u-f u-f-ac">
                         { userInfo.isLogin 
                             ? 
@@ -94,8 +90,27 @@ class Header extends Component {
                         { userInfo.isLogin ? <span className="msg-dot"></span> : '' }
                     </div>
                 </div>
-            )
-        }
+            </>
+        );
+               
+    }
+
+    // 跳转到消息列表页面
+    goMsg = ()=> {
+        const { history } = this.props
+        history.push('/informs') 
+    }
+
+    // 跳转到设置页面
+    goSet = ()=> {
+        const { history } = this.props
+        history.push('/set') 
+    }
+
+    // 跳转到登录页面
+    goLogin = ()=> {
+        const { history } = this.props
+        history.push('/login') 
     }
 
     // 用户信息
